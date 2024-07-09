e9dda7eeee19412920f7be6f890cabd8d15d6ee7
Mateus Ceci <mateusceci@gmail.com>
2024-07-09 19:21:03 -0300

    add geolocation

--- src/app/(tabs)/icon.tsx
+++ src/app/(tabs)/icon.tsx
@@ -1,9 +1,62 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
+import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
+import { useEffect, useState } from "react";
 
-export default function Icon () {
+export default function Icon() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [region, setRegion] = useState<Region | undefined>(undefined);
+
+  async function requestLocationPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+      setRegion({
+        latitude: currentPosition.coords.latitude,
+        longitude: currentPosition.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.042,
+      });
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        setLocation(response);
+      }
+    );
+  }, []);
+
   return (
     <View className="flex flex-1 items-center justify-center text-xl">
-        <Text>Icon</Text>
+      {location && (
+        <MapView
+          className="flex-1 w-full"
+          provider={PROVIDER_GOOGLE}
+          region={region}
+          showsUserLocation
+          showsMyLocationButton
+          loadingEnabled
+        />
+      )}
     </View>
-  )
+  );
 }
