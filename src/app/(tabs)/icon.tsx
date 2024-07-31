adc4ac70390e746bfcb6c200973dc6ee3aace203
Mateus Ceci <mateusceci@gmail.com>
2024-07-31 13:52:13 -0300

    create bottom sheet and add dummy data

--- src/app/(tabs)/icon.tsx
+++ src/app/(tabs)/icon.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import {
   requestForegroundPermissionsAsync,
   getCurrentPositionAsync,
@@ -6,12 +6,20 @@ import {
   watchPositionAsync,
   LocationAccuracy,
 } from "expo-location";
-import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
-import { useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE, Region } from "react-native-maps";
+import { useEffect, useMemo, useState } from "react";
+import SearchInput from "../../components/SearchInput";
+import TattooArtists from "../../dummyData/tattooArtists.json";
+import TattooArtistListItem from "../../components/map/TattooArtistListItem";
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Icon() {
   const [location, setLocation] = useState<LocationObject | null>(null);
   const [region, setRegion] = useState<Region | undefined>(undefined);
+  const [selectArtist, setSelectArtist] = useState(null);
+
+  const snapPoints = useMemo(() => [80, "50%", "90%"], []);
 
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
@@ -46,17 +54,59 @@ export default function Icon() {
   }, []);
 
   return (
-    <View className="flex flex-1 items-center justify-center text-xl">
-      {location && (
-        <MapView
-          className="flex-1 w-full"
-          provider={PROVIDER_GOOGLE}
-          region={region}
-          showsUserLocation
-          showsMyLocationButton
-          loadingEnabled
-        />
-      )}
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View className="flex flex-1 items-center justify-center text-xl">
+        <SearchInput />
+        {location && (
+          <MapView
+            className="flex-1 w-full"
+            provider={PROVIDER_GOOGLE}
+            region={region}
+            showsUserLocation
+            showsMyLocationButton
+            loadingEnabled
+          >
+            {TattooArtists.map((tattooArtist) => (
+              <Marker
+                onPress={() => setSelectArtist(tattooArtist)}
+                key={tattooArtist.id}
+                coordinate={{
+                  latitude: tattooArtist.latitude,
+                  longitude: tattooArtist.longitude,
+                }}
+                title={tattooArtist.name}
+                description="Testing"
+              >
+                <View className="bg-white p-2 rounded-xl border border-gray-500">
+                  <Text className="font-bold">{tattooArtist.name}</Text>
+                </View>
+              </Marker>
+            ))}
+          </MapView>
+        )}
+        {selectArtist && (
+          <TattooArtistListItem
+            artist={selectArtist}
+            containerStyle={{
+              position: "absolute",
+              bottom: 90,
+              right: 20,
+              left: 20,
+            }}
+          />
+        )}
+
+        <BottomSheet snapPoints={snapPoints} index={0} >
+          <Text className="text-center font-bold text-xl mb-4 mt-2">
+            Over {TattooArtists.length} places
+          </Text>
+          <BottomSheetFlatList
+            data={TattooArtists}
+            contentContainerStyle={{ gap: 10, padding: 10 }}
+            renderItem={({ item }) => <TattooArtistListItem artist={item} />}
+          />
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 }
