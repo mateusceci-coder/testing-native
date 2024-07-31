1fe027d30eca78bdb468df753ff12e62b66dec79
Mateus Ceci <mateusceci@gmail.com>
2024-07-31 13:52:37 -0300

    add margin top

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,20 +4,20 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function SearchInput() {
   return (
-    <View>
-    <Text className="font-bold ml-2 mb-2">Estilo de tatuagem</Text>
-    <View className="flex-row items-center gap-3">
-      <View
-        className="rounded-full border border-gray-300 bg-white p-3 w-10/12 flex-row items-center gap-1"
-        style={styles.shadow}
-      >
-        <View className="flex-row">
-          <MaterialIcons name="search" size={24} color="black" />
-          <TextInput placeholder="Colorida, Linhas finas, Oriental, Blackwork... " />
+    <View className="mt-12 p-2 w-full">
+      <Text className="font-bold ml-2 mb-2">Estilo de tatuagem</Text>
+      <View className="flex-row items-center gap-3">
+        <View
+          className="rounded-full border border-gray-300 bg-white p-3 w-10/12 flex-row items-center gap-1"
+          style={styles.shadow}
+        >
+          <View className="flex-row">
+            <MaterialIcons name="search" size={24} color="black" />
+            <TextInput placeholder="Colorida, Linhas finas, Oriental, Blackwork... " />
+          </View>
         </View>
+        <FontAwesome name="filter" size={32} color="black" />
       </View>
-      <FontAwesome name="filter" size={32} color="black" />
-    </View>
     </View>
   );
 }
