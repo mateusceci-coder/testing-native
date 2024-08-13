6d9765f5fe89ec17f2cac5ee9796ac5a66b20000
Mateus Ceci <mateusceci@gmail.com>
2024-08-13 20:35:24 -0300

    add align items center to scroll view

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, FlatList } from "react-native";
 import SearchInput from "../../components/SearchInput";
 import TattooContainer from "../../components/TattooContainer";
 
-export default function Index () {
+export default function Index() {
   return (
-    <ScrollView className="flex-1 gap-2 text-xl bg-white">
-        <SearchInput />
-        <TattooContainer />
-        <TattooContainer />
-        <TattooContainer />
+    <ScrollView className="flex-1 gap-2 text-xl bg-white" contentContainerStyle={{ alignItems: "center" }} >
+      <SearchInput />
+      <TattooContainer />
+      <TattooContainer />
+      <TattooContainer />
     </ScrollView>
-  )
+  );
 }
