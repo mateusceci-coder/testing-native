b9363421324631ef9d6f5c74e624c6f9486ca6ba
Mateus Ceci <mateusceci@gmail.com>
2024-08-13 20:35:49 -0300

    build profile page with stack screens

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,33 +1,17 @@
-import { View, Text, Image, ScrollView } from "react-native";
-import ArtistProfileContainer from "../../components/Profile/ArtistProfileContainer";
-import AwardsProfile from "../../components/Profile/AwardsProfile";
-import ReviewsProfile from "../../components/Profile/ReviewsProfile";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ArtistProfilePage from "../../components/Profile/ArtistProfilePage";
+import MainProfilePage from "../../components/Profile/MainProfilePage";
+import PersonalInformation from "../../components/Profile/PersonalInformation";
 
 export default function Profile() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <ScrollView className="flex-1 p-4 mt-10">
-      <ArtistProfileContainer />
-      <View className="my-10">
-        <View className="gap-2 mb-4">
-          <View className="border-b-2">
-            <Text className="font-bold">Descrição</Text>
-          </View>
-          <Text>
-            Tenho 30 anos, trabalho com black-art, linhas finas. Ganhei 5
-            prêmios de melhor tattoo no PIPIPI POPOPO
-          </Text>
-        </View>
-        <View className="gap-2 mb-4">
-          <Text className="font-bold">Idiomas</Text>
-          <Text>Português, Inglês, Francês e Espanhol</Text>
-        </View>
-        <View className="gap-2 mb-10">
-          <Text className="font-bold">Localização</Text>
-          <Text>Florianópolis - SC</Text>
-        </View>
-      </View>
-      <AwardsProfile />
-      <ReviewsProfile />
-    </ScrollView>
+    <Stack.Navigator initialRouteName="Perfil">
+      <Stack.Screen name="Perfil" component={MainProfilePage} />
+      <Stack.Screen name="Perfil do Artista" component={ArtistProfilePage} />
+      <Stack.Screen name="Informações Pessoais" component={PersonalInformation} />
+    </Stack.Navigator>
   );
 }
