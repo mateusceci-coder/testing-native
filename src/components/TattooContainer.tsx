c8947874523ce2b258c66854ed6d330798716f71
Mateus Ceci <mateusceci@gmail.com>
2024-08-13 20:36:44 -0300

    remove fixed width from image

--- src/components/TattooContainer.tsx
+++ src/components/TattooContainer.tsx
@@ -3,15 +3,18 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function TattooContainer() {
   return (
-    <View className="mt-10">
+    <View className="mt-10 items-center px-4 py-1">
       <Image
         source={require("../../public/tattoo-search.png")}
-        className="rounded-xl w-[420px]"
+        className="rounded-xl"
       />
       <View className="flex-row">
         <View className="flex-row gap-8 items-center justify-between flex-1">
-          <View className="flex-row items-center gap-1 ">
-            <Image source={require("../../public/profile.png")} className="h-20 w-20" />
+          <View className="flex-row items-center gap-1">
+            <Image
+              source={require("../../public/profile.png")}
+              className="h-16 w-16"
+            />
             <View>
               <Text className="font-bold text-xl">Zé da Tainha</Text>
               <Text>
