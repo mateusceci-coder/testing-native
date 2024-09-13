3fc2561ae92074fb82db8976d1c6d8fbff76b0e9
Mateus Ceci <mateusceci@gmail.com>
2024-09-12 21:06:49 -0300

    change to tattoo container

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,11 +4,38 @@ import TattooContainer from "../../components/TattooContainer";
 
 export default function Index() {
   return (
-    <ScrollView className="flex-1 gap-2 text-xl bg-white" contentContainerStyle={{ alignItems: "center" }} >
+    <ScrollView
+      className="flex-1 gap-2 text-xl bg-white px-4"
+      contentContainerStyle={{ alignItems: "center" }}
+    >
       <SearchInput />
-      <TattooContainer />
-      <TattooContainer />
-      <TattooContainer />
+      <TattooContainer
+        image={"../../public/bigTattoo.png"}
+        name="Zé da Tainha"
+        profile="../../../public/profile.png"
+        price={1200}
+        rating={4.5}
+        specialties="Black Work, Oriental"
+        tattoo="Fechamento de braço estilo oriental"
+      />
+      <TattooContainer
+        image={"../../public/bigTattoo.png"}
+        name="Zé da Tainha"
+        profile="../../../public/profile.png"
+        price={1200}
+        rating={4.5}
+        specialties="Black Work, Oriental"
+        tattoo="Fechamento de braço estilo oriental"
+      />
+      <TattooContainer
+        image={"../../public/bigTattoo.png"}
+        name="Zé da Tainha"
+        profile="../../../public/profile.png"
+        price={1200}
+        rating={4.5}
+        specialties="Black Work, Oriental"
+        tattoo="Fechamento de braço estilo oriental"
+      />
     </ScrollView>
   );
 }
