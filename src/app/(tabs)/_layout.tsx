0d508eeb18b1b922ac272749c3c6cf9955a1817a
Mateus Ceci <mateusceci@gmail.com>
2024-09-12 21:06:30 -0300

    change path of image

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,45 +1,57 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome'
-import { Tabs } from 'expo-router'
-import { Image } from 'react-native'
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
+import { Image } from "react-native";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: false }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
       <Tabs.Screen
-      name='index'
-      options={{
-        title:"Procurar",
-        tabBarIcon: ({ color }) => <FontAwesome size={28} name='search' color={color}
-     /> }}
+        name="index"
+        options={{
+          title: "Procurar",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="search" color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
-      name='wishlist'
-      options={{
-        title: "Whishlist",
-        tabBarIcon: ({color}) => <FontAwesome size={28} name='heart' color={color} />
-      }} />
+        name="wishlist"
+        options={{
+          title: "Whishlist",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="heart" color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
-      name='icon'
-      options={{
-        tabBarIcon: ({size=28, color}) => (
-          <Image
-          source={require('../../../public/tattoo-machine.png')}
-          style={{width: size, height: size, tintColor: color}}
-          />
-        )
-      }} />
+        name="icon"
+        options={{
+          tabBarIcon: ({ size = 28, color }) => (
+            <Image
+              source={require("../../images/tattoo-machine.png")}
+              style={{ width: size, height: size, tintColor: color }}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
-      name='chat'
-      options={{
-        title: "Conversar",
-        tabBarIcon: ({color}) => <FontAwesome size={28} name='comment' color={color} />
-      }} />
+        name="chat"
+        options={{
+          title: "Conversar",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="comment" color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
-      name='profile'
-      options={{
-        title: "Perfil",
-        tabBarIcon: ({color}) => <FontAwesome size={28} name='user' color={color} />
-      }} />
+        name="profile"
+        options={{
+          title: "Perfil",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
