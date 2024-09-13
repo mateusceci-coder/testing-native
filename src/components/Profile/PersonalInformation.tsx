c30e510e4fa0f0c08ad641e987406a9a6a47c658
Mateus Ceci <mateusceci@gmail.com>
2024-09-12 21:09:37 -0300

    change screen to use react-hook-form

--- src/components/Profile/PersonalInformation.tsx
+++ src/components/Profile/PersonalInformation.tsx
@@ -1,48 +1,38 @@
 import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import { Button, Text, TextInput, View } from "react-native";
 
 export default function PersonalInformation() {
   const initialInformations = [
-    {
-      title: "Nome",
-      value: "João Manezinho",
-    },
-    {
-      title: "Nome de Preferência",
-      value: "Sardona",
-    },
+    { title: "Nome", value: "João Manezinho", name: "nome" },
+    { title: "Nome de Preferência", value: "Sardona", name: "nomePreferencia" },
     {
       title: "Número de Telefone",
       value: "+55 11 99999-9999",
+      name: "telefone",
     },
-    {
-      title: "Email",
-      value: "xxxxx@email.com",
-    },
-    {
-      title: "Endereço",
-      value: "Não fornecido",
-    },
+    { title: "Email", value: "xxxxx@email.com", name: "email" },
+    { title: "Endereço", value: "Não fornecido", name: "endereco" },
     {
       title: "Contato de Emergência",
       value: "Não fornecido",
+      name: "contatoEmergencia",
     },
     {
       title: "Documento de identificação oficial",
       value: "Não fornecido",
+      name: "documento",
     },
   ];
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedInformations, setEditedInformations] = useState([
-    ...initialInformations,
-  ]);
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: initialInformations.reduce(
+      (acc, item) => ({ ...acc, [item.name]: item.value }),
+      {}
+    ),
+  });
 
-  const handleInputChange = (text, index) => {
-    const newInformations = [...editedInformations];
-    newInformations[index].value = text;
-    setEditedInformations(newInformations);
-  };
+  const [isEditing, setIsEditing] = useState(false);
 
   const handleEdit = () => {
     setIsEditing(true);
@@ -50,43 +40,41 @@ export default function PersonalInformation() {
 
   const handleCancel = () => {
     setIsEditing(false);
+    reset(); // Resetar os valores para os iniciais
   };
 
-  const handleConfirm = () => {
+  const handleConfirm = (data) => {
+    console.log("Dados confirmados:", data);
     setIsEditing(false);
   };
 
   return (
-    <View className="flex-1 pt-12 px-4">
+    <View className="flex-1 mt-12 px-4">
       <View className="items-center">
         {!isEditing ? (
           <Button title="Editar Informações" onPress={handleEdit} />
         ) : (
           <View className="flex-row">
-            <Button title="Confirmar" onPress={handleConfirm} />
+            <Button title="Confirmar" onPress={handleSubmit(handleConfirm)} />
             <View style={{ width: 10 }} />
             <Button title="Cancelar" onPress={handleCancel} />
           </View>
         )}
       </View>
       <View className="mt-8">
-        {editedInformations.map((info, index) => (
+        {initialInformations.map((info, index) => (
           <View
             key={index}
             className="flex-row justify-between py-4 items-center border-b border-gray-500"
           >
             <Text className="font-bold">{info.title}</Text>
             {isEditing ? (
-              <TextInput
-                style={{
-                  borderWidth: 1,
-                  borderColor: "gray",
-                  padding: 5,
-                  flex: 1,
-                  marginLeft: 10,
-                }}
-                value={info.value}
-                onChangeText={(text) => handleInputChange(text, index)}
+              <Controller
+                name={info.name}
+                control={control}
+                render={({ field: { onChange, value } }) => (
+                  <TextInput value={value} onChangeText={onChange} />
+                )}
               />
             ) : (
               <Text>{info.value}</Text>
