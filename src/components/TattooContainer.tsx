46f977c41a0fb153544bdc36168ec544d83a767c
Mateus Ceci <mateusceci@gmail.com>
2024-09-12 21:07:29 -0300

    change tattoo container to be component

--- src/components/TattooContainer.tsx
+++ src/components/TattooContainer.tsx
@@ -1,39 +1,45 @@
 import { View, Image, Text } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import { TattooContainerType } from "@/types/TattooContainerType";
 
-export default function TattooContainer() {
+export default function TattooContainer({
+  name,
+  specialties,
+  tattoo,
+  price,
+  image,
+  rating,
+  profile,
+}: TattooContainerType) {
   return (
-    <View className="mt-10 items-center px-4 py-1">
+    <View className="items-center mb-8">
       <Image
-        source={require("../../public/tattoo-search.png")}
-        className="rounded-xl"
+        source={require("../images/big-Tattoo.png")}
+        className="object-contain mb-2"
       />
-      <View className="flex-row">
-        <View className="flex-row gap-8 items-center justify-between flex-1">
-          <View className="flex-row items-center gap-1">
-            <Image
-              source={require("../../public/profile.png")}
-              className="h-16 w-16"
-            />
-            <View>
-              <Text className="font-bold text-xl">Zé da Tainha</Text>
-              <Text>
-                <Text className="font-bold">Especialidade:</Text> Blackwork,
-                Oriental
-              </Text>
-              <Text>
-                <Text className="font-bold">Tattoo:</Text> Fechamento de braço
-                estilo oriental
-              </Text>
-              <Text>
-                <Text className="font-bold">Preço:</Text>R$ 1200
-              </Text>
-            </View>
-          </View>
+      <View className="flex-row justify-between items-center w-full">
+        <View className="flex-row gap-1">
+          <Image
+            source={require("../images/profile.png")}
+            className="w-20 h-20 rounded-full"
+          />
           <View>
-            <FontAwesome name="star" size={28} color="#EEAD2D" />
-            <Text className="text-xl font-bold">5,0</Text>
+            <Text className="font-bold">{name}</Text>
+            <Text>
+              <Text className="font-bold">Especialidades:</Text> {specialties}
+            </Text>
+            <Text>
+              <Text className="font-bold">Tattoo:</Text> {tattoo}
+            </Text>
+            <Text>
+              <Text className="font-bold">Preço:</Text> R$ {price}
+            </Text>
           </View>
+        </View>
+        <View className="items-center">
+          <AntDesign name="star" size={32} color="#EEAD2D" />
+          <Text className="font-bold text-xl">{rating}</Text>
         </View>
       </View>
     </View>
